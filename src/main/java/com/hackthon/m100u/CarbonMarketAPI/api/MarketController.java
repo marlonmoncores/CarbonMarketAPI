f6c1552a0e385c3bb8131643dbc571a2bb72e1b6
Marlon Monçores <marlon.moncores@m4u.com.br>
2019-10-26 06:56:05 -0300

    add mock response

--- src/main/java/com/hackthon/m100u/CarbonMarketAPI/api/MarketController.java
+++ src/main/java/com/hackthon/m100u/CarbonMarketAPI/api/MarketController.java
@@ -1,6 +1,7 @@
 package com.hackthon.m100u.CarbonMarketAPI.api;
 
 import com.hackthon.m100u.CarbonMarketAPI.api.to.MarketBuyInputTO;
+import com.hackthon.m100u.CarbonMarketAPI.api.to.MarketBuyOutputTO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.*;
 public class MarketController {
 
     @PostMapping(path = "/user/buy")
-    public ResponseEntity<MarketBuyInputTO> saveUserBuy(@RequestBody MarketBuyInputTO marketBuyInputTO, @RequestHeader(name = "idMarket") Integer idMarket){
-        System.out.println(idMarket);
-        return ResponseEntity.ok().body(marketBuyInputTO);
+    public ResponseEntity<MarketBuyOutputTO> saveUserBuy(@RequestBody MarketBuyInputTO marketBuyInputTO, @RequestHeader(name = "idMarket") Integer idMarket){
+        MarketBuyOutputTO marketBuyOutputTO = new MarketBuyOutputTO();
+        marketBuyOutputTO.setGradeghg("A");
+        marketBuyOutputTO.setTotalghg(12345);
+        return ResponseEntity.ok().body(marketBuyOutputTO);
     }
 }
---
+++ src/main/java/com/hackthon/m100u/CarbonMarketAPI/api/to/MarketBuyOutputTO.java
@@ -0,0 +1,23 @@
+package com.hackthon.m100u.CarbonMarketAPI.api.to;
+
+public class MarketBuyOutputTO {
+
+    private long totalghg;
+    private String gradeghg;
+
+    public long getTotalghg() {
+        return totalghg;
+    }
+
+    public void setTotalghg(long totalghg) {
+        this.totalghg = totalghg;
+    }
+
+    public String getGradeghg() {
+        return gradeghg;
+    }
+
+    public void setGradeghg(String gradeghg) {
+        this.gradeghg = gradeghg;
+    }
+}
