e076d55aa9f896787c6824d4bf921be23efeeff6
Ahmed Yarub Hani Al Nuaimi <ahmedyarub@yahoo.com>
2019-10-27 00:32:51 -0300

    muito mais coisas

--- src/main/java/com/hackthon/m100u/CarbonMarketAPI/api/security/WebSecurityConfig.java
+++ src/main/java/com/hackthon/m100u/CarbonMarketAPI/api/security/WebSecurityConfig.java
@@ -1,13 +1,19 @@
 package com.hackthon.m100u.CarbonMarketAPI.api.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -20,7 +26,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-     http.cors().and()
+        http.cors().and()
                 .csrf().disable()
                 // make sure we use stateless session; session won't be used to store user's state.
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
